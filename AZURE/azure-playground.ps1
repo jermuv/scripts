bc16e59eddee3723a40d991f514a9fbe80f52ed4
JERMU VIRTANEN <jermuv@googlemail.com>
2022-05-01 17:28:25 +0300

    vähä päivityksiä

--- AZURE/azure-playground.ps1
+++ AZURE/azure-playground.ps1
@@ -1,3 +1,5 @@
 Get-AzResourceGroup | select-object -Property resourcegroupname, location | where-object { $_.location -ne "eastus2" } | where-object { $_.location -ne "eastus" }
 
 Get-AzResource | Where-Object { $_.location -EQ "westus" }
+
+Get-AzResource | Select-Object -Property ResourceType -unique | sort-object -Property ResourceType
