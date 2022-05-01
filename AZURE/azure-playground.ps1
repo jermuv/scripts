f31d2de83c4e901b9b776e3ea539f74242f47b97
JERMU VIRTANEN <jermuv@googlemail.com>
2022-05-01 16:30:00 +0300

    second commit

--- AZURE/azure-playground.ps1
+++ AZURE/azure-playground.ps1
@@ -1,1 +1,3 @@
 Get-AzResourceGroup | select-object -Property resourcegroupname, location | where-object { $_.location -ne "eastus2" } | where-object { $_.location -ne "eastus" }
+
+Get-AzResource | Where-Object { $_.location -EQ "westus" }
