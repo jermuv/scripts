d789e9869f7922ef03755566fb3e822d89ac080c
Jermu Virtanen <jermuv@googlemail.com>
2022-05-21 01:03:12 +0300

    select -> select-object

--- LABRA/fix-timezone.ps1
+++ LABRA/fix-timezone.ps1
@@ -1,2 +1,2 @@
-$a = get-adcomputer -filter * -properties name | select name
+$a = get-adcomputer -filter * -properties name | select-object name
 Invoke-command -computername $a.name  -scriptblock { tzutil /s "FLE Standard Time" }
