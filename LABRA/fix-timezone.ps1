d877b9307fb2a74a207874bbe6785acb3025cb05
Jermu Virtanen <jermuv@googlemail.com>
2022-05-21 00:08:09 +0300

    typo

--- LABRA/fix-timezone.ps1
+++ LABRA/fix-timezone.ps1
@@ -1,2 +1,2 @@
 $a = get-adcomputer -filter * -properties name | select name
-Invoke-command -computername a.name  -scriptblock { tzutil /s "FLE Standard Time" }
+Invoke-command -computername $a.name  -scriptblock { tzutil /s "FLE Standard Time" }
