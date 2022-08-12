0f5d3fe8b6dc2bffd2fc1e6c7aa6c6622c5021c4
Jermu Virtanen <jermuv@googlemail.com>
2022-08-13 01:31:53 +0300

    regionit käyttöön

--- AZURE/azure-playground.ps1
+++ AZURE/azure-playground.ps1
@@ -1,5 +1,18 @@
+# visual studio code tip: f8 -> run selected row(s)
+
+#region connect azure account
+Connect-AzAccount
+#endregion
+
+#region demo
 Get-AzResourceGroup | select-object -Property resourcegroupname, location | where-object { $_.location -ne "eastus2" } | where-object { $_.location -ne "eastus" }
-
 Get-AzResource | Where-Object { $_.location -EQ "westus" }
-
 Get-AzResource | Select-Object -Property ResourceType -unique | sort-object -Property ResourceType
+#endregion
+
+#region azuread
+Get-AzADGroup
+#endregion azuread
+
+Get-AzADUser | select -Property UserPrincipalName, type, status, accountenabled | where-object accountenabled -ne 'False'
+Get-AzADUser | select -Property UserPrincipalName, type, status, accountenabled | where-object accountenabled -eq 'False'
