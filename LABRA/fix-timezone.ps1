985e22a540b92b35666fbaa4ab044aabefc327b1
Jermu Virtanen <jermuv@googlemail.com>
2022-09-08 11:24:40 +0300

    fixtimezone updated

--- LABRA/fix-timezone.ps1
+++ LABRA/fix-timezone.ps1
@@ -1,2 +1,3 @@
+# AD powershell module needs to be present - DC or "tools" server
 $a = get-adcomputer -filter * -properties name | select-object name
 Invoke-command -computername $a.name  -scriptblock { tzutil /s "FLE Standard Time" }
