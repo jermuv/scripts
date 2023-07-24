5678e43bac128d1853495e4e44042e35b6c4a12b
Jermu Virtanen <jermuv@googlemail.com>
2023-07-24 10:19:29 +0300

    restructured

--- TESTS/duplikaattien-selvitystä.ps1
+++ TESTS/duplikaattien-selvitystä.ps1
@@ -4,32 +4,35 @@
 
 # Defining variables here
 $strOriginalFolder = Read-Host -Prompt "Provide original folder here"
-$resultsfolder = "c:\temp"
+
+# testing if the folder exists
+$strOriginalFolderExists = Test-Path -path $strOriginalFolder
+if ($strOriginalFolderExists -eq $false) {
+    write-host "Error: original folder do not exist"
+    exit
+}
+
+$resultsfolder = Read-Host -Prompt "Provide results folder here"
+
+# testing if the results folder exists
+$strResultsFolderExists = Test-Path -Path $resultsfolder
+if ($strResultsFolderExists -eq $false) {
+    Write-Host "Error: results folder do not exist"
+    exit
+}
 
 # getting suffix for the files
 $strSuffix = Read-Host -Prompt "Provide suffix for the log files"
 
-# testing if the folder exists
-$strOriginalFolderExists = Test-Path -path $strOriginalFolder
-# // debugging purposes // Write-Host "You gave folder $strOriginalFolder and does it exist? $strOriginalFolderExists" 
+# Just list subfolders under the "root" of search folder
+$listofsubfolders = Get-ChildItem -Path $strOriginalFolder -Attributes directory
 
-# if folder exists, then proceeding
-if ($strOriginalFolderExists -eq $true) {
-    # Just list subfolders under the "root" of search folder
-    $listofsubfolders = Get-ChildItem -Path $strOriginalFolder -Attributes directory
-
-    # for easier comparison, each of the subfolder under "root" will get own .csv file which can be used against excel file
-    foreach ($subfolder in $listofsubfolders) {
-
-        $subFolder = $subfolder.name
-                
-        # finally, list the files and calculate hashes - dump the results as a csv file
-        Get-ChildItem -Path $strOriginalFolder\$subFolder -Recurse -File -Force | Get-FileHash -Algorithm MD5 | Export-Csv -Path $resultsfolder\$strSuffix$subfolder.csv -Delimiter "|" -UseQuotes Never        
-    }
-
-    write-host "Task completed - results can be found from $resultsfolder"
+# for easier comparison, each of the subfolder under "root" will get own .csv file which can be used against excel file
+foreach ($subfolder in $listofsubfolders) {
+    $subFolder = $subfolder.name
+            
+    # finally, list the files and calculate hashes - dump the results as a csv file
+    Get-ChildItem -Path $strOriginalFolder\$subFolder -Recurse -File -Force | Get-FileHash -Algorithm MD5 | Export-Csv -Path $resultsfolder\$strSuffix$subfolder.csv -Delimiter "|" -UseQuotes Never        
 }
-else
-{
-    Write-Host "Error: defined folder did not exist"
-}
+
+write-host "Task completed - results can be found from $resultsfolder"
