85cecdfa07bc6eea816889d1d7af6a3e4ae13daa
Jermu Virtanen <jermuv@googlemail.com>
2023-07-24 10:06:57 +0300

    updated english variable names

--- TESTS/duplikaattien-selvitystä.ps1
+++ TESTS/duplikaattien-selvitystä.ps1
@@ -1,33 +1,32 @@
 # purpose of the script:
 # dump file name and hash of the file to the CSV file
-# reaosn for this: 2 almost identical onedrive subfolders and to be decided what files and folders are identical (to be deleted for example)
+# reason for this: 2 almost identical onedrive subfolders and to be decided what files and folders are identical (to be deleted for example)
 
 # Defining variables here
-$strOriginalFolder = Read-Host -Prompt "Gimme original folder here"
+$strOriginalFolder = Read-Host -Prompt "Provide original folder here"
 
 # getting suffix for the files
-$strSuffix = Read-Host -Prompt "Gimme suffix for the extracted files"
+$strSuffix = Read-Host -Prompt "Provide suffix for the log files"
 
 # testing if the folder exists
 $strOriginalFolderExists = Test-Path -path $strOriginalFolder
-Write-Host "You gave folder $strOriginalFolder and does it exist? $strOriginalFolderExists" 
+# // debugging purposes // Write-Host "You gave folder $strOriginalFolder and does it exist? $strOriginalFolderExists" 
 
 # if folder exists, then proceeding
 if ($strOriginalFolderExists -eq $true) {
     # Just list subfolders under the "root" of search folder
-    $dirikat = Get-ChildItem -Path $strOriginalFolder -Attributes directory
+    $listofsubfolders = Get-ChildItem -Path $strOriginalFolder -Attributes directory
 
     # for easier comparison, each of the subfolder under "root" will get own .csv file which can be used against excel file
-    foreach ($dirikka in $dirikat) {
+    foreach ($subfolder in $listofsubfolders) {
 
-        $subFolder = $dirikka.name
+        $subFolder = $subfolder.name
                 
         # finally, list the files and calculate hashes - dump the results as a csv file
         Get-ChildItem -Path $strOriginalFolder\$subFolder -Recurse -File -Force | Get-FileHash -Algorithm MD5 | Export-Csv -Path c:\temp\$strSuffix$subfolder.csv -Delimiter "|" -UseQuotes Never
-
     }
 }
 else
 {
-    Write-Host "Folder did not exist"
+    Write-Host "Error: defined folder did not exist"
 }
