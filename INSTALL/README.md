fd127d641560c33f62ee409fe7e2b9dd5c15018c
Jermu Virtanen <jermuv@googlemail.com>
2024-12-08 13:20:34 +0200

    tiny winget script

---
+++ INSTALL/README.md
@@ -0,0 +1,7 @@
+# Info
+
+download the script by command
+
+```
+curl -o winget-apps.ps1 https://github.com/jermuv/scripts/blob/master/INSTALL/winget-apps.ps1
+```
---
+++ INSTALL/winget-apps.ps1
@@ -0,0 +1,8 @@
+Winget install --id Microsoft.Powershell --Source winget
+Winget install --id Microsoft.VisualStudioCode --Source winget
+Winget install --id git.git  --Source winget
+winget install -e --id Spotify.Spotify  --Source winget
+winget install -e --id Yubico.Authenticator  --Source winget
+winget install --id microsoft.powertoys --source winget  --Source winget
+winget install -e --id JanDeDobbeleer.OhMyPosh  --Source winget
+Winget install --id Mozilla.Firefox.ESR --source winget  --Source winget
