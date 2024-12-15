cdff4f834e8ea43c6626f8af43de919168bba501
Jermu Virtanen <jermuv@googlemail.com>
2024-12-15 18:05:52 +0200

    added greenshot for winget-apps.ps1

--- INSTALL/winget-apps.ps1
+++ INSTALL/winget-apps.ps1
@@ -5,6 +5,7 @@ Winget install --id Microsoft.VisualStudioCode --Source winget
 Winget install --id git.git  --Source winget
 winget install -e --id Spotify.Spotify  --Source winget
 winget install -e --id Yubico.Authenticator  --Source winget
-winget install --id microsoft.powertoys --source winget  --Source winget
+winget install --id microsoft.powertoys --source winget
 winget install -e --id JanDeDobbeleer.OhMyPosh  --Source winget
-Winget install --id Mozilla.Firefox.ESR --source winget  --Source winget
+Winget install --id Mozilla.Firefox.ESR --source winget
+winget install --id greenshot.greenshot --source winget
